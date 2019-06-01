9822210e1527ae8734f5edb048b0eacb3d4fb563
Collin Bezrouk <collin.j.bezrouk@gmail.com>
2019-05-31 20:19:06 -0600

    Updated propagation test to use an RK78 integrator instead of a fixed time-step integrator.

--- test/prop_test.cpp
+++ test/prop_test.cpp
@@ -8,13 +8,17 @@ using namespace boost::numeric::odeint;
 
 int main() {
 
+	// Define types and steppers for the integrator.
+	typedef vector<double> vec_type;
+	typedef runge_kutta_fehlberg78 <vec_type> error_stepper;
+	controlled_runge_kutta < error_stepper > controlled_stepper;
+
 	vector<double> x = { 8.34, 0.0, 0.0, 0.0, 0.262773, 0.0 };
 	printf("\nEQUATIONS OF MOTION:\n");
 	printf("  X(t= 0): %.3f, %.3f, %.3f, %.6f, %.6f, %.6f\n", x[0], x[1], x[2], x[3], x[4], x[5]);
 
-
 	// Propagate the state.
-	size_t steps = integrate(eqns_of_motion, x, 0.0, 90.0, 0.1);
+	size_t steps = integrate_adaptive(controlled_stepper, eqns_of_motion, x, 0.0, 90.0, 0.1);
 
 	// Print results.
 	printf("  X(t=90): %.3f, %.3f, %.3f, %.6f, %.6f, %.6f\n", x[0], x[1], x[2], x[3], x[4], x[5]);
@@ -34,7 +38,7 @@ int main() {
 	printf("  X(t= 0): %.3f, %.3f, %.3f, %.6f, %.6f, %.6f\n", x_stm[0], x_stm[1], x_stm[2], x_stm[3], x_stm[4], x_stm[5]);
 
 	// Propagate the trajectory with an STM.
-	steps = integrate(eqns_of_motion_stm, x_stm, 0.0, 90.0, 0.1);
+	steps = integrate_adaptive(controlled_stepper, eqns_of_motion_stm, x_stm, 0.0, 90.0, 0.1);
 
 	 // Create a matrix to store the STM for printing purposes.
  	vector<double>::const_iterator first = x_stm.begin() + 6;
